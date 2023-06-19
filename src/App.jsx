3cd43da636462581cc87f6a4d023725f9ffda264
Cristhian Villa <crvm1993@gmail.com>
2023-06-19 09:42:34 -0500

    primera prueba

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import Cartelera from "./hoteles/Cartelera";
+import lupa from './inconos/lup.svg';
+import logo from './img/logo.svg'
+
+function App() {
+  
+
+
+  // La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
+  const [data, setData] = useState([]);
+
+  // Función para traer los datos de "stays.json".
+  const getData = async () => {
+    // Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
+    try {
+      const res = await fetch("stays.json");
+      const resJson = await res.json();
+      // Aquí guardamos los datos de "stays.json" en la variable data.
+      setData(resJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
+  useEffect(() => {
+     getData();
+  }, []);
+
+  // Puedes ver la variable data en consola.
+  console.log(data);
+  return (
+    <>
+      <section className="total">
+        <div className="contenedor-princ">
+          <div className="contenedor-sec1">
+            <div>
+              <img src={logo} alt="logo" />
+            </div>
+
+            <div className="buscador">
+              <input className="pais" type="text" />
+              <div className="personas">
+                <input className="cant-per" type="text" />
+                <button className="botoncito"><img src={lupa} className="lupa"/></button>
+              </div>
+            </div>
+          </div>
+          <div className="contenedor-sec2">
+            <div className="tituprim">
+              <h1 className="titulo-conte">Stays in Finland</h1>
+              <span>12+ stays</span>
+            </div>
+            <div className="contenedor-cart">
+              {/* Aquí te dejo un ejemplo de cómo podrías imprimir varios elementos a la vez. */}
+              {data.map((el, index) => (
+                <>
+                  <Cartelera 
+                  src={el.photo}
+                  type={el.type}
+                  beds={el.beds}
+                  superHost={el.superHost}
+                  rating={el.rating}
+                  title={el.title}
+                  key={index} />
+                  
+                </>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/hoteles/Cartelera.jsx
@@ -0,0 +1,34 @@
+import "./Cartelera.css"
+import star  from '../inconos/star.svg'
+
+function Cartelera(props) {
+  return (
+    <>
+      <div className='cartelera'>
+        
+        <img className='img' src={props.src} alt="alternado" />
+        
+        <div className='detalles'>
+            <div className={props.superHost ?'host': ""}>
+                {props.superHost ? "SUPERHOST":""}
+            </div>
+         
+          <div className='mini-detalle1'>
+            <h4 className="letra">{props.type} </h4>
+            <h4 className="letra">.{props.beds} beds</h4>
+          </div>
+
+          <div className='mini-detalle2'>
+            <img className='img1' src={star} alt="" />
+            <h3 className="rating">{props.rating}</h3>
+          </div>
+        </div>
+        <div className='titulo'>
+            <h2 className='titulo1'>{props.title}</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cartelera
